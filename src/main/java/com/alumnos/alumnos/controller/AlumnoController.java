d7b845dd8a7969714da755bfaf6fbe84953ea15e
Juan Pulgar <jpulgar1978@gmail.com>
2025-04-23 12:40:47 -0400

    se agrega enpoint de listado de alumnos

--- src/main/java/com/alumnos/alumnos/controller/AlumnoController.java
+++ src/main/java/com/alumnos/alumnos/controller/AlumnoController.java
@@ -1,5 +1,14 @@
 package com.alumnos.alumnos.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
 public class AlumnoController {
 
+    @GetMapping("/api/v1/alumnos")
+    public String getAlumnos() {
+        return "LISTADO DE ALUMNOS";
+    }
+    
 }
